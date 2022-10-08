bba068752edf53b70b9bff647db811530f5cd1f4
Mohanvamshi123 <113008509+Mohanvamshi123@users.noreply.github.com>
2022-10-08 14:36:00 +0530

    Add files via upload

---
+++ day5 9vote.py
@@ -0,0 +1,6 @@
+age=input("enter the age of the person:")
+if(age>=eighteen):
+    print("the person is eligible to vote:")
+else:
+        a=18-eighteen
+        print(a,"years are left to be eligible")
