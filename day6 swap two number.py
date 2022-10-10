f216b0d879be3cff5590db05d89e47b86adb1a0e
Mohanvamshi123 <113008509+Mohanvamshi123@users.noreply.github.com>
2022-10-10 09:42:59 +0530

    Add files via upload

---
+++ day6 swap two number.py
@@ -0,0 +1,7 @@
+x=input("eneter the valu of x:")
+y=input("eneter the value of y:")
+temp=x
+x=y
+y=temp
+print("the value of x after swapping:{}".format(x))
+print("the value of y after swapping:{}".format(y))
